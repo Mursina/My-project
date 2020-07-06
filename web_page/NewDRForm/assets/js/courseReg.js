575e000a53f638b743c46a6babdeadd9bc005408
bonita1 <bonita1@vm5.us-central1-a.c.bonita-273710.internal>
2020-07-06 03:53:19 +0000

    Error: Failed to create a BarResource for form custompage_NewHODForm

---
+++ web_page/NewDRForm/assets/js/courseReg.js
@@ -0,0 +1,17 @@
+function(){
+    alert("hi");
+//     if( $data.application && $data.application_Courses ){
+// 	//attach lazy references variables to parent variables
+// 	$data.application.Courses = $data.application_Courses;
+// 	return {
+// 		//map application variable to expected task contract input
+// 		applicationInput: {
+// 			nameWithInitials: $data.application.nameWithInitials !== undefined ? $data.application.nameWithInitials : "abbjkkk",
+// 			Courses: $data.application.Courses.map( it => ({
+// 				persistenceId_string: it.persistenceId_string !== undefined ? it.persistenceId_string : null
+// 			}))
+// 		}
+// 	}
+// }
+//   return "kjkjjkkj";
+}
---
+++ web_page/NewDRForm/assets/js/test1.js
@@ -0,0 +1,36 @@
+function hi(name){
+    alert('Welcome ' + name);  
+}
+
+
+
+/*$data.application.nameWithInitials = "mursina"
+var r = {
+	    "number": 5
+	};
+
+//if( $data.application && $data.application_Courses ){
+	//attach lazy references variables to parent variables
+    console.log("Hi");
+	$data.application.Courses = $data.application_Courses;
+	$data.application.nameWithInitials = $data.batchMap.regNumId.name;
+
+	
+	console.log($data.application);
+	var m = {
+		//map application variable to expected task contract input
+		applicationInput: {
+			Courses: $data.application.Courses.map( it => ({
+				persistenceId_string: it.persistenceId_string !== undefined ? it.persistenceId_string : null
+			}))
+		}
+	}
+	
+	r = {
+	    "number": 6
+	};
+	console.log(r);
+	console.log(m);
+	return r;
+//}
+*/
